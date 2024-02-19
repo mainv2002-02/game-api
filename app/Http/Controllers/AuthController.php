16d6093a7e17ce8f0f956c71a8ef3230c97953cb
Mai Nguyen <mainv@vng.com.vn>
2024-02-19 11:31:29 +0700

    add routers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class AuthController extends Controller
+{
+    public function login(): View
+    {
+        return view('auth/login');
+    }
+
+    public function doLogin():View
+    {
+        return view('auth/login');
+    }
+}
---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class GameController extends Controller
+{
+    public function show(): View
+    {
+        return view('game/question-detail');
+    }
+
+    public function answer(): View
+    {
+        return view('game/question-detail');
+    }
+}
