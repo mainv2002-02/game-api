b962e9b16c6ddf75423d79e057c97a46f83012e7
lamchunghoang <chunglam89@gmail.com>
2024-04-06 10:07:39 +0700

    new page gift css version

--- resources/views/home/gift2.blade.php
+++ resources/views/home/gift2.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Home Page')
 
 @section('style-libraries')
-    <link rel="stylesheet" href="{{ url('assets/css/home/gift2.css') }}?v=0.1">
+    <link rel="stylesheet" href="{{ url('assets/css/home/gift2.css?v=1.2') }}">
 @stop
 
 @section('styles')
